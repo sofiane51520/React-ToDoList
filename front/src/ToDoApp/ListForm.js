46e7a3ccf8e9fd04f3c2919f785d6506a3005114
sofiane51520 <45100006+sofiane51520@users.noreply.github.com>
2021-05-25 16:45:40 +0200

    correcting close animation and adding button in form

--- front/src/ToDoApp/ListForm.js
+++ front/src/ToDoApp/ListForm.js
@@ -12,18 +12,18 @@ const ListForm = ({display, toggleDisplay,submitFct, data = null})=> {
         reset()
     }
 
-    if (data){
-        setValue('name', data.name)
-        setValue('description', data.description)
-        setValue('img', data.img)
-    }
+    // if (data){
+    //     if (data.name.length) setValue('name', data.name)
+    //     if (data.description.length) setValue('description', data.description)
+    //     if (data.img.length) setValue('img', data.img)
+    // }
 
     async function submit(){
         submitFct({name:getValues('name'),description:getValues('description'),img:getValues('img')})
     }
 
     return (
-        <form className={`${display ?'max':'min'}`} onSubmit={handleSubmit(onSubmit)}>
+        <form className={`${display ?'max':'minimize'}`} onSubmit={handleSubmit(onSubmit)}>
             {errors.name && <span>{errors.name.message}</span>}
             <div className={'inputContainer'}>
                 <input type={'text'} name={'name'} placeholder={'Nom'} {...register('name',{required: 'Vous devez entrer une valeur'})}/>
@@ -31,7 +31,9 @@ const ListForm = ({display, toggleDisplay,submitFct, data = null})=> {
                 <input type={'text'} name={'img'} placeholder={'Lien de l\'image'}  {...register('img')}/>
             </div>
             <div className={'btnContainer'}>
-                <FaCheckCircle size={30} className={'doneBtn'} type={'submit'}  disabled={isSubmitting} onClick={onSubmit}/>
+                {/*<FaCheckCircle size={30} className={'doneBtn'} type={'submit'}  disabled={isSubmitting} onClick={onSubmit}/>*/}
+
+                <button type={"submit"}> <FaCheckCircle size={30} className={'doneBtn'} type={'submit'}  disabled={isSubmitting}/></button>
                 <FaTimes size={30} className={'deleteBtn'} onClick={toggleDisplay}/>
             </div>
         </form>
